e1e3ca2685b8188700c06291f00badab6c86fe02
Nazar <nazariy84@gmail.com>
2017-09-25 18:28:36 +0300

    fix registration

--- src/app/auth/components/login.component.ts
+++ src/app/auth/components/login.component.ts
@@ -4,6 +4,7 @@ import { MdDialog, MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
 
 import { User } from '../../common/models/models';
 import { RegisterComponent } from './register.component';
+import { UserService } from '../../common/services/user.service';
 
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
@@ -17,21 +18,32 @@ export class LoginComponent implements OnInit {
     action: string;
     signUp: boolean = false;
     myForm: FormGroup;
-    email: FormControl;
+    //email: FormControl;
+    login: FormControl;    
     password: FormControl;
-    MDdialog: MdDialog;   
+    MDdialog: MdDialog; 
+    user: User   
 
     constructor(
         public dialogRef: MdDialogRef<LoginComponent>,
-        @Inject(MD_DIALOG_DATA) public data: any
+        @Inject(MD_DIALOG_DATA) public data: any,
+        private userService: UserService
+        
     ){
         this.action = data.action;
-        this.MDdialog = data.signUp;        
+        this.MDdialog = data.signUp; 
+        this.user={Login:"non"} as User;       
     }
 
     onNoClick(): void {
         this.dialogRef.close();
-    }    
+    }
+    LogIn():void{ 
+        this.user.Login=this.login.value;
+        this.user.Password=this.password.value;
+        console.log(this.user);
+        this.userService.loginUser(this.user);
+        }    
 
     Register(): void {        
         this.MDdialog.open(RegisterComponent, {
@@ -53,19 +65,19 @@ export class LoginComponent implements OnInit {
     }
 
     createFormControls(){
-        this.email= new FormControl('', [
+        this.login= new FormControl('', [
             Validators.required,
-            Validators.pattern(EMAIL_REGEX)
+            //Validators.pattern(EMAIL_REGEX)
         ]);
         this.password=new FormControl('', [
             Validators.required,
-            Validators.minLength(8)            
+            Validators.minLength(6)            
         ]);
     }
 
     createForm(){
         this.myForm = new FormGroup({            
-            email: this.email,
+            email: this.login,
             password: this.password            
           });
     }
--- src/app/common/services/user.service.ts
+++ src/app/common/services/user.service.ts
@@ -9,6 +9,7 @@ import { handleError } from '../functions/functions';
 @Injectable()
 export class UserService {
     private UsersUrl = 'http://localhost:37271/Catalog/GetUsers';
+    private AccountUrl='http://localhost:37271/Account/';
 
     constructor(private http: Http) { }
 
@@ -36,4 +37,24 @@ export class UserService {
             .then(response => response.json() as Statistic)
             .catch(handleError);
     }
+    loginUser(user:User){
+     
+                return this.http.post(this.AccountUrl+"SignIn",user)
+                    .toPromise()
+                    .then(response =>{
+                        var us=response.json() as User;
+                        alert("Hello "+us.Login);
+                    } )
+                    .catch(handleError);
+    }
+    registerUser(user:User){
+        
+                   return this.http.post(this.AccountUrl+"SignUp",user)
+                       .toPromise()
+                       .then(response =>{
+                           var us=response.json() as User;
+                           alert("You are registered with login: "+us.Login);
+                       } )
+                       .catch(handleError);
+       }
 }
