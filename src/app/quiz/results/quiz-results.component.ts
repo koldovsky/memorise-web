52bb88f0172aea5b945e399f38e47900cd4e19e7
JmilSv <Jmil.sv@gmail.com>
2017-10-23 23:24:07 +0300

    add icons

--- src/app/quiz/results/quiz-results.component.ts
+++ src/app/quiz/results/quiz-results.component.ts
@@ -60,15 +60,15 @@ export class QuizResultsComponent implements OnInit {
   checkCard(card: Card): string {
      if (card.CardType.Name === 'Words input') {
        if (this.wordInputs[card.Id].IsRight) {
-          return 'Right';
+          return 'done';
        }else {
-        return 'Wrong';
+        return 'close';
       }
      }else if (card.CardType.Name === 'Code input') {
        if (this.codeAnswers[card.Id].IsRight) {
-         return 'Right';
+         return 'done';
        } else {
-         return 'Wrong';
+         return 'close';
        }
      }else {
     let customerRightAnswersCount = 0;
@@ -83,9 +83,9 @@ export class QuizResultsComponent implements OnInit {
       }
     });
     if (customerRightAnswersCount === rightAnswersCount) {
-      return 'Right';
+      return 'done';
     } else {
-      return 'Wrong';
+      return 'close';
     }
    }
   }
