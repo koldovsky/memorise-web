31d063ed51a43781cf95b3f0620deaeca7cab0ce
NastyaKolomoets <nastya.kolomoets@gmail.com>
2017-10-23 19:44:41 +0300

    Implement subscription and unsubscription for courses in course details and for decks.

--- src/app/catalog/decks/decks.component.ts
+++ src/app/catalog/decks/decks.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
-import { Deck, Category } from '../../common/models/models';
+import { Deck, Category, DeckSubscription, Statistics } from '../../common/models/models';
 import { DeckService } from '../../common/services/deck.service';
 import { MessageService } from '../../common/services/message.service';
 import { CategoryService } from '../../common/services/category.service';
+import { AuthService } from '../../common/services/auth.service';
+import { UserSubscriptionsService } from '../../common/services/user-subscriptions.service';
+import { StatisticsService } from '../../common/services/statistics.service';
+import { handleError } from '../../common/functions/functions';
 
 @Component({
     selector: 'app-decks',
@@ -12,31 +17,96 @@ import { CategoryService } from '../../common/services/category.service';
 })
 
 export class DecksComponent implements OnInit {
-    constructor(private deckService: DeckService,
-        private messageService: MessageService,
-        private categoryService: CategoryService) { }
-   
+    constructor(
+        private deckService: DeckService,
+        private categoryService: CategoryService,
+        private authService: AuthService,
+        private subscriptionsService: UserSubscriptionsService,
+        private statisticsService: StatisticsService,
+        private route: ActivatedRoute) { }
+
+    currentUserLogin: string;
+    subscriptions: DeckSubscription[];
+    statistics: Statistics[];
     decks: Deck[];
 
     ngOnInit(): void {
-        if (this.messageService.temp) {
-            const category = this.messageService.temp as Category;
-            this.categoryService.getDecksByCategory(category.Linking)
-                .then(decks => this.decks = decks);
-        } else {
-            this.deckService.getDecks()
-                .then(decks => this.decks = decks);
-        }
-
-        this.messageService.getMessage().subscribe(data => {
-            if (data) {
-                const category = data as Category;
-                this.categoryService.getDecksByCategory(category.Linking)
-                    .then(decks => this.decks = decks);
-            } else {
-                this.deckService.getDecks()
-                    .then(decks => this.decks = decks);
+        this.route.paramMap
+        .switchMap((params: ParamMap) => {
+            const category = params.get('category');
+            return category === 'Any'
+                ? this.deckService.getDecks()
+                : this.categoryService.getDecksByCategory(category);
+        }).subscribe(decks => {
+            this.decks = decks;
+            if (this.authService.checkIfIsAuthorized()) {
+                this.currentUserLogin = this.authService.getCurrentUserLogin();
+                this.subscriptionsService.getDeckSubscriptions(this.currentUserLogin)
+                    .subscribe(subscriptions => {
+                        this.subscriptions = subscriptions;
+                        this.checkSubscriptions();
+                    });
             }
         });
+    }
+
+    checkSubscriptions() {
+        this.decks.forEach(deck => {
+            deck.IsSubscribed = this.subscriptions
+                .find(x => x.DeckId === deck.Id)
+                ? true
+                : false;
+        });
+    }
+
+    subscribeToDeck(course: Deck): void {
+        const subscription = {
+            Rating: -1,
+            UserLogin: this.currentUserLogin,
+            DeckId: course.Id
+        };
+        this.subscriptionsService.subscribeToDeck(subscription)
+            .subscribe(
+            x => {
+                this.subscriptions.push(x);
+                course.IsSubscribed = true;
+                this.createDeckStatistics(course);
+            },
+            err => handleError);
+    }
+
+    unsubscribeFromDeck(course: Deck): void {
+        const subscription = this.subscriptions.find(x => x.DeckId === course.Id);
+        if (subscription) {
+            this.subscriptionsService.unsubscribeFromDeck(subscription.Id)
+                .subscribe(
+                x => {
+                    course.IsSubscribed = false;
+                    this.subscriptions = this.subscriptions.filter(s => s !== x);
+                    this.deleteDeckStatistics(course);
+                },
+                err => handleError
+                );
+        }
+    }
+
+    createDeckStatistics(course: Deck): void {
+        const subscriptionStatistics = {
+            UserLogin: this.currentUserLogin,
+            ItemId: course.Id
+        };
+        this.statisticsService
+            .createStatisticsForDeck(subscriptionStatistics)
+            .subscribe();
+    }
+
+    deleteDeckStatistics(course: Deck): void {
+        this.statisticsService
+            .getStatisticsByUserAndDeck(this.currentUserLogin, course.Id)
+            .subscribe(statistics => {
+                statistics.forEach(x => {
+                    this.statisticsService.deleteStatistics(x.Id).subscribe();
+                });
+            });
     }
 }
