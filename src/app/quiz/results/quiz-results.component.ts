7ce1a29bc4ccb1429fc74953811839444b885f18
NastyaKolomoets <nastya.kolomoets@gmail.com>
2017-10-26 15:53:23 +0300

    Remove white spase on home page. Add footer.

--- src/app/quiz/results/quiz-results.component.ts
+++ src/app/quiz/results/quiz-results.component.ts
@@ -80,7 +80,6 @@ export class QuizResultsComponent implements OnInit {
         ? this.checkCodeInput(card)
         : this.checkTestInput(card);
 
-        console.log(isAnswerCorrect);
     return isAnswerCorrect;
   }
 
