3f84e6543d76f4058f0b8243be50c49b3d5783b4
Nazar <nazariy84@gmail.com>
2017-10-30 14:19:44 +0200

    fix commit

--- src/app/common/services/interceptor.service.ts
+++ src/app/common/services/interceptor.service.ts
@@ -40,7 +40,7 @@ export class InterceptorService implements HttpInterceptor {
 
     return next.handle(request).do((event: HttpEvent<any>) => {
       if (event instanceof HttpResponse) {
-        // do stuff with success response
+
       }
     }, (err) => {
       if (err instanceof HttpErrorResponse) {
